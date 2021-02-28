f4d4fb4339b0e023936b7fa0cc414a727945bbb6
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-02-28 21:06:36 +0100

    test now

--- routes/bitcore-lib.js
+++ routes/bitcore-lib.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-var Insight = require('bitcore-insight').Insight;
-var bitcore = require('bitcore-lib')
+var explo = require('bitcore-explorers')
 const base58 = require('base58');
-let insight = new Insight('testnet');
 
 router.get('/genprivatekey', (req, res, next) => {
     var shell = {}
@@ -36,33 +34,10 @@ router.get('/genprivatekeywif', (req, res, next) => {
 })
 
 router.get('/bitcointranscation', (req, res, next) => {
+    var insight = new explo.Insight()
     const myAdress = 'mpKdzhfbrCgEyUPSRPny7F3H5ANg457Yp7';
-    insight.getUtxos(myAdress, (err, utxos) => {
-        if(err){ 
-          console.log('err',err);
-          return err;
-        }else { 
-            console.log('utxos',utxos);
-            // use the UTXOs to create transaction with bitcore Transaction object
-            /**let tx = bitcore.Transaction();
-            tx.from(utxos);
-            tx.to(addressTo, amount);
-            tx.change(myAddress);
-            tx.fee(fee);
-            tx.sign(privateKey);
-            tx.serialize();
-            
-            // Broadcast your transaction to the Bitcoin network
-            insight.broadcast(tx.toString(), (error, txid) => {
-                if (error) {
-                    return error;
-                } else {
-                  // Your Transaction Id
-                    console.log(txid)
-                }
-            })**/
-        }
-    });
+    insight.address(myAdress, (error, result) => { console.log(error,result) })
+    
 })
 
 
