582cd6cbad2e8c5002666cbd6d450917634b680d
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-02-28 20:55:08 +0100

    ready to push bitcoin transaction

--- routes/bitcore-lib.js
+++ routes/bitcore-lib.js
@@ -1,46 +1,68 @@
 const express = require("express");
 const router = express.Router();
-const bitcore = require('bitcore-lib');
 var Insight = require('bitcore-insight').Insight;
-
+var bitcore = require('bitcore-lib')
+const base58 = require('base58');
+let insight = new Insight('testnet');
 
 router.get('/genprivatekey', (req, res, next) => {
-    let insight = new Insight('testnet');
-    const privateKey = new bitcore.PrivateKey();
-    console.log(privateKey.toAddress());
+    var shell = {}
+
+    // Generate a new Bitcoin address
+    var slug = 'AlwaysBCoding!BitcoinScreencast1'
+    var hash = bitcore.crypto.Hash.sha256(new Buffer(slug))
+    var bn = bitcore.crypto.BN.fromBuffer(hash)
+    var pKey = bitcore.PrivateKey(bn)
+    var addr = pKey.toAddress();
+    console.log(addr)
 })
 
 router.get('/genprivatekeysha256', (req, res, next) => {
-    let value = Buffer.from('cat horse shoe lightning awesome bitcoin');
-    let hash = bitcore.crypto.Hash.sha256(value);
-    let bn = bitcore.crypto.BN.fromBuffer(hash);
-    const privateKey = new bitcore.PrivateKey(bn);
-    console.log(privateKey);
-    const address = privateKey.toAddress();
-    console.log(address.hashBuffer.toString());
+    var input = bitcore.crypto.Random.getRandomBuffer(32);
+    var hash = bitcore.crypto.Hash.sha256(input);
+    var bn = bitcore.crypto.BN.fromBuffer(hash);
+    //private key
+    var pk = new bitcore.PrivateKey(bn);
+    //public key
+    var addy = pk.toAddress();
+    console.log('private key : ' + pk + ' , Public key : ' + addy);
 })
 
 router.get('/genprivatekeywif', (req, res, next) => {
-    let insight = new Insight('testnet');
-    const wif = 'cQ4tueVFAueyXsS7DhQfU1bhP35NKaGmA9EYWufNthD1XCDXhddy';
-    const privateKey = new bitcore.PrivateKey(wif);
-    const address = privateKey.toAddress();
-    insight.address(address,(err, address) => {
-        if (err) {
-            console.log(err);
-          // Handle errors...
-        } else {
-            console.log(address)
-          return address;
-        }
-      })
+    var pk = new bitcore.PrivateKey()
+    var WIF = pk.toWIF();
+    var address = pk.toAddress('testnet');
+    console.log('private key : ' + pk + ' , WIF key : ' + WIF + ' , Public key : ' + address);
 })
 
-router.get('/genprivatekey1', (req, res, next) => {
-    const rand_buffer = bitcore.crypto.Random.getRandomBuffer(32);
-    const rand_number = bitcore.crypto.BN.fromBuffer(rand_buffer);
-    const address = new bitcore.PrivateKey('testnet').toWIF();
-    console.log(address);
+router.get('/bitcointranscation', (req, res, next) => {
+    const myAdress = 'mpKdzhfbrCgEyUPSRPny7F3H5ANg457Yp7';
+    insight.getUtxos(myAdress, (err, utxos) => {
+        if(err){ 
+          console.log('err',err);
+          return err;
+        }else { 
+            console.log('utxos',utxos);
+            // use the UTXOs to create transaction with bitcore Transaction object
+            /**let tx = bitcore.Transaction();
+            tx.from(utxos);
+            tx.to(addressTo, amount);
+            tx.change(myAddress);
+            tx.fee(fee);
+            tx.sign(privateKey);
+            tx.serialize();
+            
+            // Broadcast your transaction to the Bitcoin network
+            insight.broadcast(tx.toString(), (error, txid) => {
+                if (error) {
+                    return error;
+                } else {
+                  // Your Transaction Id
+                    console.log(txid)
+                }
+            })**/
+        }
+    });
 })
 
 
