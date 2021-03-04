43df9dbfd132fcfff1ec91c003daa3287a5711d6
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-03-04 20:20:59 +0100

    improvement

--- routes/api.js
+++ routes/api.js
@@ -16,61 +16,61 @@ router.get("/test", (req, res) => res.json({ msg: "backend works" }));
 
 router.get("/getblockcount", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getbestblockhash", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getconnectioncount", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getdifficulty", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getblockchaininfo", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getmininginfo", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getaccountaddress", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 router.get("/getpeerinfo", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getrawmempool", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getnewaddress", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":["-addresstype legacy"]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/walletlock", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getbalance", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.post("/sendtoaddress", (req, res) => {
@@ -116,32 +116,32 @@ router.post("/listunspent", (req, res) => {
 
 router.get("/walletpassphrase", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":["${PASS + " 3600"}"]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/dumpprivkey/:address", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":["${req.params.address}"]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getblock/:hash", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[${req.params.hash}]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getblockhash/:index", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[${req.params.index}]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/getrawtransaction/:id", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[${req.params.id}]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 router.get("/decoderawtransaction/:hex", (req, res) => {
   var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"${req.url.split('/')[1]}","params":[${req.params.hex.toString()}]}`;
-  getInfo(res,dataString);
+  response(res,dataString);
 });
 
 function prepareRequest(res,dataString,callback){
@@ -160,36 +160,44 @@ function prepareRequest(res,dataString,callback){
   request(options, callback);
 }
 
-function getInfo(res,dataString){
-  console.log(dataString);
-  var options = {
-    url: `http://167.99.213.37:80/wallet/bitexplode-test1`,
-    method: "POST",
-    headers: headers,
-    auth:{
-      user:USER,
-      pass:PASS,
-      sendImmediately:false
-    },
-    body: dataString
-  };
+function getInfo(dataString){
+  return new Promise((resolve,reject) => {
+    var options = {
+      url: `http://167.99.213.37:80/wallet/bitexplode-test1`,
+      method: "POST",
+      headers: headers,
+      auth:{
+        user:USER,
+        pass:PASS,
+        sendImmediately:false
+      },
+      body: dataString
+    };
+  
+    callback = (error, response, body) => {
+      if (error || response.statusCode != 200){
+        console.log('here')
+        resolve({err:"Something Went Wrong",method:url,status:response.statusCode,message:JSON.parse(body)});
+      }
+      if (!error && response.statusCode == 200) {
+        console.log('here2')
+        const data = JSON.parse(body);
+        console.log(data.result);
+        resolve({data:data});
+      }
+    };
+    request(options, callback);
+  })
+}
 
-  callback = (error, response, body) => {
-    console.log("err",error);
-    console.log('response',response.statusCode);
-    console.log('body',body);
-    if (error || response.statusCode != 200){
-      console.log('here')
-      res.json({err:"Something Went Wrong",method:url,status:response.statusCode,message:JSON.parse(body)})
-    }
-    if (!error && response.statusCode == 200) {
-      console.log('here2')
-      const data = JSON.parse(body);
-      console.log(data.result);
-      res.json({data:data});
-    }
-  };
-  request(options, callback);
+function response(res,dataString) {
+  getInfo(dataString)
+    .then(result => {
+      res.json(result)
+    })
+    .catch(err => {
+      res.json(err);
+    })
 }
 
 
